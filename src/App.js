d36e592feaf8d9557184fd0ddb032f93c747873c
Ron Hagafny <ronhagafny20@gmail.com>
2022-01-14 18:43:15 +0200

    some more progress

--- src/App.js
+++ src/App.js
@@ -3,28 +3,40 @@ import { useQuery } from "react-query"
 import axios from "axios"
 
 function App() {
+    const [pokemon, setPokemon] = React.useState("charizard")
+
     return (
         <div>
-            <PokemonSearch />
+            <input
+                value={pokemon}
+                type="text"
+                onChange={(e) => setPokemon(e.target.value)}
+            />
+            <PokemonSearch pokemon={pokemon} />
         </div>
     )
 }
 
-function PokemonSearch() {
+function PokemonSearch({ pokemon }) {
     const queryInfo = useQuery("pokemon", async () => {
         await new Promise((resolve) => setTimeout(resolve, 1000))
-        // return axios
-        //     .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-        //     .then((res) => res.data.results)
+        return axios
+            .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+            .then((res) => res.data.results)
     })
 
+    console.log(queryInfo)
     return queryInfo.isLoading ? (
         "Loading..."
     ) : queryInfo.isError ? (
         queryInfo.error.message
     ) : (
         <div>
-            Show the pokemon
+            {queryInfo.data?.sprites?.front_default ? (
+                <img src={queryInfo.data.sprites.front_default} alt="pokemon" />
+            ) : (
+                "Pokemon not found"
+            )}
             <br />
             {queryInfo.isFetching ? "Updating..." : null}
         </div>
