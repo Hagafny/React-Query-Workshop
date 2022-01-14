7dd5f91bc34175aaea6c422eb47acb70c846d1ed
Ron Hagafny <ronhagafny20@gmail.com>
2022-01-14 15:45:07 +0200

    completed

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
 
     return queryInfo.isLoading ? (
         "Loading..."
+    ) : queryInfo.isError ? (
+        queryInfo.error.message
     ) : (
         <div>
             {queryInfo.data.map((pokemon) => (
