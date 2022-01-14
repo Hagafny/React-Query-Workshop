6ffc1972698d74d187204aa165834a851ff6e7e4
Ron Hagafny <ronhagafny20@gmail.com>
2022-01-14 18:10:45 +0200

    setting it up

--- src/App.js
+++ src/App.js
@@ -1,45 +1,29 @@
 import React from "react"
 import { useQuery } from "react-query"
 import axios from "axios"
-import { FETCH_POKEMON_URL } from "./consts"
 
 function App() {
     return (
         <div>
-            <Count />
-            <PokemonList queryKey="pokemon" />
+            <PokemonSearch />
         </div>
     )
 }
 
-function Count() {
-    const queryInfo = usePokemon()
-    return <h3>You are looking at {queryInfo.data?.length} pokemon</h3>
-}
-
-function usePokemon() {
-    return useQuery("pokemon", async () => {
+function PokemonSearch({ pokemon }) {
+    const queryInfo = useQuery("pokemon", async () => {
         await new Promise((resolve) => setTimeout(resolve, 1000))
-        return axios.get(FETCH_POKEMON_URL).then((res) => res.data.results)
+        return axios
+            .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+            .then((res) => res.data.results)
     })
-}
-
-function PokemonList() {
-    const queryInfo = usePokemon()
 
     return queryInfo.isLoading ? (
         "Loading..."
     ) : queryInfo.isError ? (
         queryInfo.error.message
     ) : (
-        <div>
-            {queryInfo.data.map((pokemon) => (
-                <div key={pokemon.name}>{pokemon.name}</div>
-            ))}
-
-            <br />
-            {queryInfo.isFetching ? "Updating..." : null}
-        </div>
+        <div> Show the pokemon</div>
     )
 }
 
