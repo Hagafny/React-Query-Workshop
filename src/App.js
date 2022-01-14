6e03aec213dd6533cac1ad1e83c4b7fa0113c0c2
Ron Hagafny <ronhagafny20@gmail.com>
2022-01-14 15:22:03 +0200

    so far

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import axios from "axios"
 import { FETCH_POKEMON_URL } from "./consts"
 
 function App() {
-    const queryInfo = useQuery("pokemon", () =>
+    const queryInfo = useQuery("pokemon", async () => {
+        await new Promise((resolve) => setTimeout(resolve, 1000))
         axios.get(FETCH_POKEMON_URL).then((res) => res.data.results)
-    )
+    })
 
     console.log(queryInfo.isSuccess)
 
-    return (
+    return queryInfo.isLoading ? (
+        "Loading..."
+    ) : (
         <div>
-            {queryInfo.data?.map((pokemon) => (
+            {queryInfo.data.map((pokemon) => (
                 <div key={pokemon.name}>{pokemon.name}</div>
             ))}
         </div>
