021bec6a4c55de8d55e1ac78523b578f826db020
Ron Hagafny <ronhagafny20@gmail.com>
2022-01-14 23:19:43 +0200

    working on initial

--- src/App.js
+++ src/App.js
@@ -1,52 +1,81 @@
 import React from "react"
 import { useQuery } from "react-query"
 import axios from "axios"
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    useParams,
+} from "react-router-dom"
 
 function App() {
-    const [pokemon, setPokemon] = React.useState("")
+    return (
+        <Router>
+            <Routes>
+                <Route path="/:postId" element={<Post />} />
+                <Route path="/" element={<Posts />} />
+            </Routes>
+        </Router>
+    )
+}
+
+function Posts() {
+    const postsQuery = useQuery("posts", async () => {
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+        return axios
+            .get("https://jsonplaceholder.typicode.com/posts")
+            .then((res) => res.data)
+    })
 
     return (
         <div>
-            <input
-                value={pokemon}
-                type="text"
-                onChange={(e) => setPokemon(e.target.value)}
-            />
-            <PokemonSearch pokemon={pokemon} />
+            <h1>Posts {postsQuery.isFetching ? "..." : null}</h1>
+            <div>
+                {postsQuery.isLoading ? (
+                    "Loading posts..."
+                ) : (
+                    <ul>
+                        {postsQuery.data.map((post) => {
+                            return (
+                                <li key={post.id}>
+                                    <Link to={`/${post.id}`}>{post.title}</Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                )}
+            </div>
         </div>
     )
 }
 
-function PokemonSearch({ pokemon }) {
-    const queryInfo = useQuery(
-        ["pokemon", pokemon],
-        async () => {
-            return axios
-                .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-                .then((res) => res.data)
-        },
-        {
-            enabled: !!pokemon,
-        }
-    )
+function Post() {
+    const { postId } = useParams()
+    const postQuery = useQuery(["post", postId], async () => {
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+        return axios
+            .get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+            .then((res) => res.data)
+    })
 
-    return queryInfo.isLoading ? (
-        "Loading..."
-    ) : queryInfo.isError ? (
-        queryInfo.error.message
-    ) : (
+    return (
         <div>
-            {queryInfo.data?.sprites?.front_default ? (
-                <img src={queryInfo.data.sprites.front_default} alt="pokemon" />
-            ) : (
-                "Pokemon not found"
-            )}
+            <Link to={"/"}>Back</Link>
             <br />
-            {queryInfo.isFetching ? "Updating..." : null}
+            <br />
+            {postQuery.isLoading ? (
+                "Loading..."
+            ) : (
+                <>
+                    {postQuery.data.title}
+                    <br />
+                    <br />
+                    {postQuery.isFetching ? "Updating..." : null}
+                </>
+            )}
         </div>
     )
 }
 
 export default App
-
-// <img src={queryInfo.data.sprites.front_default} alt="pokemon" />
