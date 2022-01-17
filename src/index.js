2ec0f4733ff398a8cf25da2358a8fe96dd0fe4c2
Ron Hagafny <ronhagafny20@gmail.com>
2022-01-18 00:08:07 +0200

    switch imnports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
-import { QueryClient, QueryClientProvider } from "react-query"
+import { QueryClientProvider, QueryClient } from "react-query"
 
 const queryClient = new QueryClient()
 
