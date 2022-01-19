f30c5e5754685ecb68f97321d89814d1247e31c9
Ron Hagafny <ronhagafny20@gmail.com>
2022-01-20 01:21:28 +0200

    chapter 2 complete

---  done/App.js
+++  done/App.js
@@ -5,41 +5,31 @@ import { sleep } from "./utils"
 const FETCH_POKEMON_URL = "https://pokeapi.co/api/v2/pokemon"
 
 function App() {
-  const [showList, setShowList] = React.useState(true)
+    const queryInfo = useQuery(
+        "pokemon",
+        async () => {
+            await sleep(1000)
+            return axios.get(FETCH_POKEMON_URL).then((res) => res.data.results)
+        },
+        {
+            refetchOnWindowFocus: false,
+            staleTime: 5000, //0 Infinity
+        }
+    )
 
-  const togglePokemonList = () => setShowList(!showList)
-
-  return (
-    <div>
-    <button onClick={togglePokemonList}>
-    {showList ? "Hide" : "Show"}
-    </button>
-    <br />
-    {showList ? <PokemonList /> : null}
-</div>
-)
-}
-
-function PokemonList() {
-  const queryInfo = useQuery("pokemon", async () => {
-    await sleep(1000)
-    return axios.get(FETCH_POKEMON_URL).then((res) => res.data.results)
-  })
-
-  return queryInfo.isLoading ? (
-    "Loading..."
-  ) : queryInfo.isError ? (
-    queryInfo.error.message
-  ) : (
-    <div>
-    {queryInfo.data.map((pokemon) => (
-        <div key={pokemon.name}>{pokemon.name}</div>
-))}
-
-<br />
-  {queryInfo.isFetching ? "Updating..." : null}
-  </div>
-)
+    return queryInfo.isLoading ? (
+        "Loading..."
+    ) : queryInfo.isError ? (
+        queryInfo.error.message
+    ) : (
+        <div>
+            {queryInfo.data.map((pokemon) => (
+                <div key={pokemon.name}>{pokemon.name}</div>
+            ))}
+            <br />
+            {queryInfo.isFetching ? "Updating..." : null}
+        </div>
+    )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import { QueryClientProvider, QueryClient } from "react-query"
-=======
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { QueryClientProvider, QueryClient } from 'react-query'
->>>>>>> 1-intro
 
 const queryClient = new QueryClient()
 
