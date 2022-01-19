a5b353b8ddf85fe84df8f60d28fa8c126237c5c6
Ron Hagafny <ronhagafny20@gmail.com>
2022-01-20 00:26:06 +0200

    change

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
 import { QueryClientProvider, QueryClient } from "react-query"
 import { ReactQueryDevtools } from "react-query/devtools"
-=======
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { QueryClientProvider, QueryClient } from 'react-query'
->>>>>>> 1-intro
 
 const queryClient = new QueryClient()
 
